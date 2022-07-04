fe9e36a4a3e8281bf32582611be98fde06e52e76
Amanda Klenk <amandamklenk3@gmail.com>
2022-07-04 16:47:37 -0400

    Added JawsDB

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,17 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-//create connection to db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//create connection to Heroku or db
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-});
-
+  });
+}
 //export
 module.exports = sequelize;
