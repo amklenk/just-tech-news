3f6cb830497b85189c14884b42ba7b07334e58b3
Amanda Klenk <amandamklenk3@gmail.com>
2022-07-05 18:37:50 -0400

    fixed User.js

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,17 @@
-//import sequelize
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-//create connection to Heroku or db
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
   });
 }
-//export
+
 module.exports = sequelize;
