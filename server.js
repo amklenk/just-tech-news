35cfd407c2369f42302adcdc8ec150b2a6ff37fe
Amanda Klenk <amandamklenk3@gmail.com>
2022-07-08 16:03:10 -0400

    created and tested helper functions, put into handlebars to optimize data presentation

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,7 +24,7 @@ const sess = {
 //session middleware
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 //handlebars middleware
 app.engine('handlebars', hbs.engine);
